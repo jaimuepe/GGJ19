e82b621a1a5ee728696470948e0955efa3d135b4
MrXuso <mrxuso@gmail.com>
2019-01-25 21:12:08 +0100

    Commit prueba.

---
+++ Assets/Scenes/SceneXuso/Scripts/InteractablObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
